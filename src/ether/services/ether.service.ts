e36569d9a030de4e2b6f76cc4888e6af8664f896
Mahit Mehta <mahitmehta@Mahits-MacBook-Air-334.local>
2021-12-25 21:40:13 -0500

    Ethereum Address Function

--- src/ether/services/ether.service.ts
+++ src/ether/services/ether.service.ts
@@ -1,6 +1,20 @@
 import { Injectable } from "@nestjs/common";
+import crypto from "crypto";
+import ethers from "ethers";
 
 @Injectable()
 export class EtherService {
     constructor() {}
+
+    public generateAddress() : { address:string, privateKey:string } {
+        const privateKey = this.generatePrivateKey();
+        const { address } = new ethers.Wallet(privateKey);
+        return { address, privateKey };
+    }
+
+    private generatePrivateKey() : string {
+        const id = crypto.randomBytes(32).toString('hex');
+        const privateKey = `0x${id}`;
+        return privateKey; 
+    }
 }
