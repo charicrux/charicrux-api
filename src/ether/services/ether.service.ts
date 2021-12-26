4432b03297af538a8ea68b043761c6d5df68edb1
Mahit Mehta <mahitmehta@Mahits-MacBook-Air-334.local>
2021-12-25 22:22:34 -0500

    Added Dynamic Contact Creator

--- src/ether/services/ether.service.ts
+++ src/ether/services/ether.service.ts
@@ -1,10 +1,28 @@
 import { Injectable } from "@nestjs/common";
 import crypto from "crypto";
 import ethers from "ethers";
+import { generateTemplateFiles, CaseConverterEnum } from "generate-template-files";
+const path = require('path');
 
 @Injectable()
 export class EtherService {
     constructor() {}
+
+    public generateDynamicContract() {
+        generateTemplateFiles([{
+            option: "Dynamic Ethereum Contract",
+            defaultCase: CaseConverterEnum.PascalCase,
+            entry: {
+                folderPath: path.join(__dirname, "../templates", "contract.sol"),
+            },
+            stringReplacers: ['__store__', { question: 'Insert model name', slot: '__model__' }],
+            output: {
+                path: path.join(__dirname, "../contracts"),
+                pathAndFileNameDefaultCase: CaseConverterEnum.KebabCase,
+                overwrite: false,
+              },
+        }])
+    }
 
     public generateAddress() : { address:string, privateKey:string } {
         const privateKey = this.generatePrivateKey();
