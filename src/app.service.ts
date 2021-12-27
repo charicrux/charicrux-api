5279e6049d8d1194fed2e7cd1b7f1f93e17b77e4
Mahit Mehta <mahitmehta@Mahits-MacBook-Air-334.local>
2021-12-27 12:27:18 -0500

    Updated Contract Template

--- src/app.service.ts
+++ src/app.service.ts
@@ -7,6 +7,11 @@ export class AppService {
     private readonly etherService: EtherService
   ) {}
   async getHello(): Promise<string> {
+    // await this.etherService.generateDynamicContract("South Brunswick School District Test", "SBSDT").then(async ({ absolutePath, outputFileName }) => {
+    //   const { bytecode, interface:contractInterface } = await this.etherService.compileSmartContractWithSolidity(absolutePath)
+    //   this.etherService.deploySmartContract(contractInterface, bytecode);
+    // });
+
     return 'Hello World!';
   }
 }
--- src/ether/services/ether.service.ts
+++ src/ether/services/ether.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from "@nestjs/common";
 import { generateTemplateFilesBatch, CaseConverterEnum } from "generate-template-files";
 import config from "../../config/index";
+import { promises as fs } from "fs";
 const HDWalletProvider = require("truffle-hdwallet-provider")
 const { exec } = require("child_process");
 const path = require('path');
@@ -8,7 +9,6 @@ const crypto = require("crypto");
 const ethers = require("ethers");
 const Web3 = require("web3");
 const solc = require("solc");
-const fs = require('fs');
 
 @Injectable()
 export class EtherService {
@@ -39,9 +39,9 @@ export class EtherService {
         return { outputFileName, absolutePath };
     }
 
-    public compileSmartContractWithSolidity(absolutePath:string) : { interface:any, bytecode:any } {
+    public async compileSmartContractWithSolidity(absolutePath:string) : Promise<{ interface:any, bytecode:any }> {
         const identifer = "OrganizationToken";
-        const source = fs.readFileSync(absolutePath, "utf8");
+        const source = await fs.readFile(absolutePath, "utf8");
         return solc.compile(source, 1).contracts[`:${identifer}`];
 
         // Once Contract is Compiled
@@ -60,6 +60,7 @@ export class EtherService {
     }
 
     public async deploySmartContract(contractInterface, bytecode) {
+        console.log("Deploying Contract...");
         // Before Deployment
         // 1. Need to Verify Token Doesn't Already Exist
 
