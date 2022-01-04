907c0dbc3960f3d7ee9d2dc32b58dcf8ef5caac8
Mahit Mehta <mahitmehta@Mahits-MacBook-Air-334.local>
2022-01-04 14:00:11 -0500

    Updated Root Endpoint Text

--- src/app.service.ts
+++ src/app.service.ts
@@ -10,6 +10,6 @@ export class AppService {
     //const privateKeyExample = "0x110526fdbe3fc57bc97044f156b71d2362c2d4976b8a3d27a07e2f525c038528";
     //const balance = await this.etherService.getWalletBalance(privateKeyExample); // config.cryptoRootWallet.privateKey
     //console.log(balance);
-    return 'All Systems Operational.';
+    return 'All Systems Operational.<br/>200 OK';
   }
 }
